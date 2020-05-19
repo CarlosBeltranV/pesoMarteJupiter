6877af56415cc126405b8c8489b728a8f7d28709
Carlos <carlos.beltran.villamizar@gmail.com>
2020-05-19 07:26:32 -0500

    Actualizado peso-marte-jupiter.js

--- src/peso-marte-jupiter.js
+++ src/peso-marte-jupiter.js
@@ -1,3 +1,3 @@
-import {PesoMarteJupiter} from 'PesoMarteJupiter.js';
+import {PesoMarteJupiter} from './PesoMarteJupiter.js';
 
 window.customElements.define('peso-marte-jupiter', PesoMarteJupiter);
